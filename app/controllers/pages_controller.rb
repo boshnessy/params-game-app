0843dd2028ba98d687d3855750ccef66ba6d3092
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-07 20:55:44 -0600

    create number guessing game

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,4 +14,27 @@ class PagesController < ApplicationController
     render json: {name: response}
   end
 
+  def guess_a_number
+    # make an answer
+    answer = 36
+    # user guesses
+    user_guess = params["guess"].to_i
+    # i compare that guess to answer
+    # p "answer"
+    # p answer
+    # p "user_guess"
+    # p user_guess # test b/c yay message not working --> one is an integer, one is a string --> convert answer to string or convert user_guess to integer
+    if answer == user_guess
+      message = "yay"
+    elsif answer > user_guess
+      message = "you guessed too low"
+    elsif answer < user_guess
+      message = "you guessed too high"
+    else
+      message = "nope, you suck"
+    end
+    # if they get it right, tell them "yay!" if they get it wrong, tell them they suck
+    render json: {message: message}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get '/name_url' => 'pages#show_name'
+  get '/name-url' => 'pages#show_name'
+  get '/number-game' => 'pages#guess_a_number'
 end
